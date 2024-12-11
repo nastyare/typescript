68c4a95a0174666b9e501b56148a76896819b792
Anastasia <anastasiya.reksius@mail.ru>
2024-12-11 12:48:19 +0700

    remove unnecessary lines

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
-
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
