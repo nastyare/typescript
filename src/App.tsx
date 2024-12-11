e297e1d49ba51403cda9bd446392d901c51a2ac8
Anastasia <anastasiya.reksius@mail.ru>
2024-12-11 12:48:33 +0700

    add toast container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./styles/base.scss";
 import "./styles/sections.scss";
 import MainSection from "./components/MainSection";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 	return (
 		<div>
 			<MainSection />
+			<ToastContainer
+				toastStyle={{ backgroundColor: "#242320", color: "#fff" }}
+				position="bottom-right"
+				autoClose={3000}
+			/>
 		</div>
 	);
 }
